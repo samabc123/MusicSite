3d456fbafa500a7bd2f5f9f356ad5a65edff26af
samabc123 <l13593596811@163.com>
2024-02-21 18:50:42 +0800

    后端的建立

---
+++ music-server/src/test/java/com/li/musicserver/MusicServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.li.musicserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
